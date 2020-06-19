9953da6c4c09452e199aea611e22aef7995e6d5b
Daniel Barter <daniel@mercury.com>
2020-06-18 17:04:24 -0700

    bump

--- c/tealeafQR.h
+++ c/tealeafQR.h
@@ -9,7 +9,8 @@ int32_t conjugateIndex (int32_t index,int32_t num_rows, int32_t num_columns);
 
 
 // amplitudes should be a pointer to an array of length
-// row_cutoff * column_cutoff + (row_cutoff - 1) * (column_cutoff - 1) 
+// row_cutoff * column_cutoff + (row_cutoff - 1) * (column_cutoff - 1)
+
 uint8_t *generateTeaLeafQR( fftw_complex *amplitudes,
                             int32_t num_rows,
                             int32_t num_columns,
